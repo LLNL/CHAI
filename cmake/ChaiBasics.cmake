c50549b64631f6dc7898932873049aee306b0f70
David Beckingsale <davidbeckingsale@gmail.com>
2017-04-27 14:11:09 -0700

    Move SPHINX_THEME

--- cmake/ChaiBasics.cmake
+++ cmake/ChaiBasics.cmake
@@ -1,7 +1,2 @@
 set (CUDA_NVCC_FLAGS -std=c++11; --expt-extended-lambda; -G; -g)
-
 #add_definitions(-DDEBUG=0)
-
-set (SPHINX_HTML_THEME "import sphinx_rtd_theme\n
-html_theme = \"sphinx_rtd_theme\"\n
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]")
