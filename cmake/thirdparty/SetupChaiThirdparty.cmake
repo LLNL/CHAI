d4d79ad2a937f1d00626a740ab196a053ed60a34
David Beckingsale <davidbeckingsale@gmail.com>
2017-10-26 15:40:48 -0700

    Remove cnmem

--- cmake/thirdparty/SetupChaiThirdparty.cmake
+++ cmake/thirdparty/SetupChaiThirdparty.cmake
@@ -41,17 +41,9 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #######################################################################
 
-message (STATUS "cnmem support is ${ENABLE_CNMEM}")
+find_package(Umpire REQUIRED)
 
-if (ENABLE_CNMEM)
-  if (NOT ENABLE_CUDA)
-    message(FATAL_ERROR "Cannot use cnmem without CUDA. Please re-configure with -DENABLE_CUDA=On")
-  endif ()
-
-  include(${CMAKE_SOURCE_DIR}/cmake/thirdparty/Findcnmem.cmake)
-
-  blt_register_library(
-    NAME cnmem
-    INCLUDES ${cnmem_INCLUDE_DIRS}
-    LIBRARIES ${cnmem_LIBRARIES})
-endif()
+blt_register_library(
+  NAME umpire
+  INCLUDES ${UMPIRE_INCLUDE_DIRS}
+  LIBRARIES ${UMPIRE_LIBRARIES})
