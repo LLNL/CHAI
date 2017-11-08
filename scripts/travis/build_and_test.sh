de71b498df6c9038898987b400b01a17cdf60507
David Beckingsale <davidbeckingsale@gmail.com>
2017-11-08 15:51:41 -0800

    Fixing typos

--- scripts/travis/build_and_test.sh
+++ scripts/travis/build_and_test.sh
@@ -28,7 +28,7 @@ if [[ "$BUILD_RAJA" == "yes" ]] ; then
   or_die cd ${TRAVIS_BUILD_DIR}/../
   or_die git clone --recursive -b develop https://github.com/LLNL/RAJA.git
   or_die cd RAJA
-  or_di mkdir build
+  or_die mkdir build
   or_die cd build
   or_die cmake -DCMAKE_CXX_COMPILER="${COMPILER}" ${CMAKE_EXTRA_FLAGS} -DENABLE_CHAI=On -Dchai_DIR=${TRAVIS_BUILD_DIR}/travis-install ../
   or_die make -j 3 VERBOSE=2
