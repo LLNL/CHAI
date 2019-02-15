28b3672a16a002d96ed51d3377f6e288af167306
Alan Dayton <dayton8@llnl.gov>
2019-02-15 14:32:51 -0800

    Added some integration tests for managed_ptr

---
+++ tests/integration/managed_ptr_tests.cpp
@@ -0,0 +1,105 @@
+// ---------------------------------------------------------------------
+// Copyright (c) 2016-2018, Lawrence Livermore National Security, LLC. All
+// rights reserved.
+//
+// Produced at the Lawrence Livermore National Laboratory.
+//
+// This file is part of CHAI.
+//
+// LLNL-CODE-705877
+//
+// For details, see https:://github.com/LLNL/CHAI
+// Please also see the NOTICE and LICENSE files.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// - Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+// - Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the
+//   distribution.
+//
+// - Neither the name of the LLNS/LLNL nor the names of its contributors
+//   may be used to endorse or promote products derived from this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+// OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+// AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+// WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// ---------------------------------------------------------------------
+#include "gtest/gtest.h"
+
+#define CUDA_TEST(X, Y)              \
+  static void cuda_test_##X##Y();    \
+  TEST(X, Y) { cuda_test_##X##Y(); } \
+  static void cuda_test_##X##Y()
+
+#include "chai/config.hpp"
+
+#include "../src/util/forall.hpp"
+
+#include "chai/managed_ptr.hpp"
+
+// Standard library headers
+#include <cstdlib>
+
+class TestBase {
+   public:
+      TestBase() {}
+
+      CHAI_HOST_DEVICE virtual int getValue(const int i) const = 0;
+};
+
+class TestDerived : public TestBase {
+   public:
+      TestDerived() : TestBase(), m_values(nullptr) {}
+      TestDerived(chai::ManagedArray<int> values) : TestBase(), m_values(values) {}
+
+      CHAI_HOST_DEVICE virtual int getValue(const int i) const { return m_values[i]; }
+
+   private:
+      chai::ManagedArray<int> m_values;
+};
+
+TEST(managed_ptr, inner_ManagedArray)
+{
+  const int expectedValue = rand();
+
+  chai::ManagedArray<int> array(1, chai::CPU);
+  array[0] = expectedValue;
+
+  chai::managed_ptr<TestDerived> derived(new TestDerived(array));
+  ASSERT_EQ(derived->getValue(0), expectedValue);
+}
+
+CUDA_TEST(managed_ptr, cuda_inner_ManagedArray)
+{
+  const int expectedValue = rand();
+
+  chai::ManagedArray<int> array(1, chai::CPU);
+  array[0] = expectedValue;
+
+  chai::managed_ptr<TestBase> derived(new TestDerived(array));
+  chai::ManagedArray<int> results(1, chai::GPU);
+  
+  forall(cuda(), 0, 1, [=] __device__ (int i) {
+    results[i] = derived->getValue(i);
+  });
+
+  results.move(chai::CPU);
+  ASSERT_EQ(results[0], expectedValue);
+}
+
