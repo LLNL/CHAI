98a56fcbda2587a718b31d80dda29e46be00048f
David Beckingsale <davidbeckingsale@gmail.com>
2019-10-08 11:02:32 -0700

    loop over multiple arrays

--- examples/context.cpp
+++ examples/context.cpp
@@ -9,14 +9,25 @@ int main()
   camp::devices::Context cuda_context{camp::devices::Cua()}; 
   camp::devices::Context host_context{camp::devices::Host()}; 
 
-  chai::ManagedArray<float> array(10);
+  std::vector< chai::ManagedArray<float> > arrays(5);
+
+  for (auto array : arrays) {
+    camp::devices::Context ctx{camp::devices::Cua()}; 
+    auto e = forall(&ctx, 0, 10, 
+        [=] CHAI_HOST_DEVICE (int i) { array[i] = i; });
+    array.move(chai::CPU, ctx);
+  }
+
+  for (auto array : arrays) {
+    camp::devices::Context ctx{camp::devices::Host{}}; 
+    auto e = forall(&ctx, 0,10, [=] CHAI_HOST_DEVICE (int i) { array[i] = 123; });
+  }
 
   std::cout << "defining lambda" << std::endl;
   auto lambda_set = [=] CHAI_HOST_DEVICE (int i) { array[i] = i; };
   auto lambda_check = [=] CHAI_HOST_DEVICE (int i) { array[i] = 123; };
 
   std::cout << "calling forall with cuda context" << std::endl;
-  auto e = forall(&cuda_context, 0, 10, lambda_set);
  
   e.wait();
   std::cout << "calling forall with host context" << std::endl;
