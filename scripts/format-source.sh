a0a897ea2480542d4fec5c25196dc1ba3fc26d2a
David Beckingsale <davidbeckingsale@gmail.com>
2019-10-16 07:25:22 -0700

    Switch to SPDX license

--- scripts/format-source.sh
+++ scripts/format-source.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+##############################################################################
+# Copyright (c) 2016-19, Lawrence Livermore National Security, LLC and CHAI
+# project contributors. See the COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+##############################################################################
 
 find . -type f -iname '*.hpp' -o -iname '*.cpp' | grep -v -e blt -e tpl | xargs clang-format -i
