0ca9c6e0f23fba28dffec3961e3099fb63181055
mdavis36 <davis291@llnl.gov>
2020-06-29 11:35:48 -0700

    Adding Resource tests.

--- tests/integration/managed_array_resource_tests.cpp
+++ tests/integration/managed_array_resource_tests.cpp
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2016-19, Lawrence Livermore National Security, LLC and CHAI
+// Copyright (c) 2016-20, Lawrence Livermore National Security, LLC and CHAI
 // project contributors. See the COPYRIGHT file for details.
 //
 // SPDX-License-Identifier: BSD-3-Clause
@@ -12,6 +12,7 @@
   static void gpu_test_##X##Y()
 
 #include "../src/util/forall.hpp"
+#include "../src/util/gpu_clock.hpp"
 
 #include "chai/ManagedArray.hpp"
 #include "chai/config.hpp"
@@ -19,48 +20,27 @@
 #ifdef CHAI_ENABLE_CUDA
 GPU_TEST(ManagedArray, Simple)
 {
-  auto callBack = [&](const chai::PointerRecord* record, chai::Action act, chai::ExecutionSpace s)
-  {
-    const size_t bytes = record->m_size;
-    printf("cback: act=%s, space=%s, bytes=%ld\n", chai::PrintAction[(int) act], chai::PrintExecSpace[(int) s], (long) bytes);
-    if (act == chai::ACTION_MOVE)
-    {
-      if (s == chai::CPU)
-      {
-        printf("Moved to host\n");
-      }
-      else if (s == chai::GPU)
-      {
-        printf("Moved to device\n");
-      }
-    }
-    if (act == chai::ACTION_FOUND_ABANDONED) {
-       printf("in abandoned!\n");
-       ASSERT_EQ(false,true);
-    }
-  };
   constexpr std::size_t ARRAY_SIZE{1024};
 
   camp::resources::Resource host{camp::resources::Host{}};
   camp::resources::Resource device{camp::resources::Cuda{}};
 
   chai::ManagedArray<double> array(ARRAY_SIZE);
-  array.setUserCallback(callBack);
 
-  forall(&host, 0, ARRAY_SIZE, [=] __host__ __device__ (int i) {
+  forall(&host, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
       array[i] = i;
   });
 
-  forall(&device, 0, ARRAY_SIZE, [=] __host__ __device__ (int i) {
+  forall(&device, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
       array[i] = array[i] * 2.0;
   });
 
-  array.move(chai::CPU, &device);
-
   // print on host
-  forall(&host, 0, ARRAY_SIZE, [=] __host__ __device__ (int i) {
+  forall(&host, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
     EXPECT_DOUBLE_EQ(array[i], i*2.0);
   });
+
+  array.free();
 }
 
 GPU_TEST(ManagedArray, SimpleWithAsyncMoveFrom)
@@ -72,18 +52,18 @@ GPU_TEST(ManagedArray, SimpleWithAsyncMoveFrom)
 
   chai::ManagedArray<double> array(ARRAY_SIZE);
 
-  forall(&host, 0, ARRAY_SIZE, [=] __host__ __device__ (int i) {
+  forall(&host, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
       array[i] = i;
   });
 
-  forall(&device, 0, ARRAY_SIZE, [=] __host__ __device__ (int i) {
+  forall(&device, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
       array[i] = array[i] * 2.0;
   });
 
   array.move(chai::CPU, &device);
 
   // print on host
-  forall(&host, 0, ARRAY_SIZE, [=] __host__ __device__ (int i) {
+  forall(&host, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
       EXPECT_DOUBLE_EQ(array[i], i*2.0);
   });
 }
@@ -97,19 +77,97 @@ GPU_TEST(ManagedArray, SimpleWithAsyncMoveTo)
 
   chai::ManagedArray<double> array(ARRAY_SIZE);
 
-  forall(&host, 0, ARRAY_SIZE, [=] __host__ __device__ (int i) {
+  forall(&host, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
       array[i] = i;
   });
 
   array.move(chai::GPU, &device);
 
-  forall(&device, 0, ARRAY_SIZE, [=] __host__ __device__ (int i) {
+  forall(&device, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
       array[i] = array[i] * 2.0;
   });
 
   // print on host
-  forall(&host, 0, ARRAY_SIZE, [=] __host__ __device__ (int i) {
+  forall(&host, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
       EXPECT_DOUBLE_EQ(array[i], i*2.0);
+  });
+
+  array.free();
+}
+
+GPU_TEST(ManagedArray, MultiStreamDepends)
+{
+  constexpr std::size_t ARRAY_SIZE{1024};
+  int clockrate{get_clockrate()}; 
+
+  camp::resources::Resource dev1{camp::resources::Cuda{}};
+  camp::resources::Resource dev2{camp::resources::Cuda{}};
+  camp::resources::Resource host{camp::resources::Host{}};
+
+  chai::ManagedArray<double> array1(ARRAY_SIZE);
+  chai::ManagedArray<double> array2(ARRAY_SIZE);
+
+  forall(&dev1, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
+      array1[i] = i;
+      gpu_time_wait_for(10, clockrate);
+  });
+
+  auto e2 = forall(&dev2, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
+      array2[i] = -1;
+      gpu_time_wait_for(20, clockrate);
+  });
+
+  dev1.wait_for(&e2);
+
+  forall(&dev1, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
+      array1[i] *= array2[i];
+      gpu_time_wait_for(10, clockrate);
+  });
+
+  array1.move(chai::CPU, &dev1);
+
+  forall(&host, 0, 10, [=] CHAI_HOST_DEVICE (int i) {
+      EXPECT_DOUBLE_EQ(array1[i], i*-1.0);
+  });
+
+  array1.free();
+  array2.free();
+}
+
+GPU_TEST(ManagedArray, MultiStreamSingleArray)
+{
+  constexpr std::size_t ARRAY_SIZE{1024};
+  int clockrate{get_clockrate()}; 
+
+  chai::ManagedArray<double> array1(ARRAY_SIZE);
+
+  camp::resources::Resource dev1{camp::resources::Cuda{}};
+  camp::resources::Resource dev2{camp::resources::Cuda{}};
+
+
+  auto e1 = forall(&dev1, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
+      if (i % 2 == 0) {
+        array1[i] = i;
+        gpu_time_wait_for(10, clockrate);
+      }
+  });
+
+  auto e2 = forall(&dev2, 0, ARRAY_SIZE, [=] CHAI_HOST_DEVICE (int i) {
+      if (i % 2 == 1) {
+        gpu_time_wait_for(20, clockrate);
+        array1[i] = i;
+      }
+  });
+
+  e1.wait();
+  e2.wait();
+
+  array1.move(chai::CPU, &dev1);
+
+  camp::resources::Resource host{camp::resources::Host{}};
+
+  forall(&host, 0, 10, [=] CHAI_HOST_DEVICE (int i) {
+      EXPECT_DOUBLE_EQ(array1[i], (double)i);
   });
 }
 #endif //#ifdef CHAI_ENABLE_CUDA
