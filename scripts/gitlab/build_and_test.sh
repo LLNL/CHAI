e3fd72665b2fb47dfc4ee4ce2481ec16f31d0fb7
Adrien M. Bernede <bernede1@llnl.gov>
2020-08-04 15:54:29 -0700

    Fix capture of spec string in script

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -41,7 +41,7 @@ then
         prefix_opt="--prefix=${prefix}"
     fi
 
-    python scripts/uberenv/uberenv.py --spec=${spec} ${prefix_opt}
+    python scripts/uberenv/uberenv.py --spec="${spec}" ${prefix_opt}
 
 fi
 
