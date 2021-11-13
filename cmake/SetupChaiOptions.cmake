a19579338796020489aa59fcc1739bf1a9c18ecd
Kristi <belcher6@rzansel61.coral.llnl.gov>
2021-11-12 16:07:05 -0800

    fixing cmake option command syntax

--- cmake/SetupChaiOptions.cmake
+++ cmake/SetupChaiOptions.cmake
@@ -4,10 +4,10 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 ############################################################################
-option(CHAI_ENABLE_HIP Off CACHE BOOL "Enable HIP")
-option(CHAI_ENABLE_GPU_SIMULATION_MODE Off CACHE BOOL "Enable GPU Simulation Mode")
-option(CHAI_ENABLE_OPENMP OFF CACHE BOOL "Enable OpenMP")
-option(CHAI_ENABLE_MPI Off CACHE BOOL "Enable MPI (for umpire replay only)")
+option(CHAI_ENABLE_HIP "Enable HIP" Off)
+option(CHAI_ENABLE_GPU_SIMULATION_MODE "Enable GPU Simulation Mode" Off)
+option(CHAI_ENABLE_OPENMP "Enable OpenMP" Off)
+option(CHAI_ENABLE_MPI "Enable MPI (for umpire replay only)" Off)
 option(CHAI_ENABLE_IMPLICIT_CONVERSIONS "Enable implicit conversions to-from raw pointers" On)
 
 option(CHAI_DISABLE_RM "Make ManagedArray a thin wrapper" Off)
@@ -18,21 +18,21 @@ option(CHAI_ENABLE_PINNED "Use pinned host memory" Off)
 option(CHAI_ENABLE_RAJA_PLUGIN "Build plugin to set RAJA execution spaces" Off)
 option(CHAI_ENABLE_GPU_ERROR_CHECKING "Enable GPU error checking" On)
 option(CHAI_ENABLE_MANAGED_PTR "Enable managed_ptr" On)
-option(CHAI_DEBUG "Enable Debug Logging.")
-option(CHAI_ENABLE_RAJA_NESTED_TEST ON CACHE BOOL "Enable raja-chai-nested-tests, which fails to build on Debug CUDA builds.")
+option(CHAI_DEBUG "Enable Debug Logging." Off)
+option(CHAI_ENABLE_RAJA_NESTED_TEST "Enable raja-chai-nested-tests, which fails to build on Debug CUDA builds." On)
 
-option(CHAI_ENABLE_TESTS On CACHE BOOL "")
-option(CHAI_ENABLE_BENCHMARKS On CACHE BOOL "Enable benchmarks")
-option(CHAI_ENABLE_EXAMPLES On CACHE BOOL "")
-option(CHAI_ENABLE_REPRODUCERS Off CACHE BOOL "")
-option(CHAI_ENABLE_DOCS Off CACHE BOOL "")
+option(CHAI_ENABLE_TESTS "Enable CHAI tests" On)
+option(CHAI_ENABLE_BENCHMARKS "Enable benchmarks" On)
+option(CHAI_ENABLE_EXAMPLES "Enable CHAI examples" On)
+option(CHAI_ENABLE_REPRODUCERS "Enable CHAI reproducers" Off)
+option(CHAI_ENABLE_DOCS "Enable CHAI documentation" Off)
 
 # options for Umpire as TPL
 set(ENABLE_GMOCK On CACHE BOOL "")
 option(CHAI_ENABLE_ASSERTS "Build Umpire with assert() enabled" On)
 set(ENABLE_GTEST_DEATH_TESTS ${CHAI_ENABLE_ASSERTS} CACHE BOOL "")
 
-option(CHAI_ENABLE_COPY_HEADERS Off CACHE BOOL "")
+option(CHAI_ENABLE_COPY_HEADERS "Enable CHAI copy headers" Off)
 
 set(ENABLE_CUDA Off CACHE BOOL "Enable CUDA")
 
