fa2fbc182e5049e6473ed52aa82800bcea4b53dd
Alan Dayton <6393677+adayton1@users.noreply.github.com>
2023-07-11 13:39:57 -0700

    Deprecate API for synchronization after device kernels (#228)

--- src/chai/ArrayManager.hpp
+++ src/chai/ArrayManager.hpp
@@ -399,11 +399,13 @@ public:
   /*!
    * \brief Turn on device synchronization after every kernel.
    */
+  [[deprecated("Set the CUDA_LAUNCH_BLOCKING or HIP_LAUNCH_BLOCKING environment variable instead")]]
   void enableDeviceSynchronize() { m_device_synchronize = true; }
 
   /*!
    * \brief Turn off device synchronization after every kernel.
    */
+  [[deprecated("Set the CUDA_LAUNCH_BLOCKING or HIP_LAUNCH_BLOCKING environment variable instead")]]
   void disableDeviceSynchronize() { m_device_synchronize = false; }
 
   /*!
