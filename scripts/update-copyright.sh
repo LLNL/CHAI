0cd1645dd642847775629db0dcb7bac5befd9795
Alan Dayton <dayton8@llnl.gov>
2024-02-16 15:55:36 -0800

    Clean up

--- scripts/update-copyright.sh
+++ scripts/update-copyright.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 
-###############################################################################
-# Copyright (c) 2016-24, Lawrence Livermore National Security, LLC
-# and other RAJA project contributors. See the RAJA/LICENSE file for details.
+##############################################################################
+# Copyright (c) 2016-24, Lawrence Livermore National Security, LLC and CHAI
+# project contributors. See the CHAI LICENSE file for details.
 #
-# SPDX-License-Identifier: (BSD-3-Clause)
-###############################################################################
+# SPDX-License-Identifier: (MIT)
+##############################################################################
 
 #=============================================================================
 # Change the copyright date in all files that contain the text
-# "the RAJA/LICENSE file", which is part of the copyright statement 
-# at the top of each RAJA file. We use this to distinguish RAJA files from 
-# that we do not own (e.g., other repos included as submodules), which we do
-# not want to modify. Note that this file and *.git files are omitted
+# "the CHAI LICENSE file", which is part of the copyright statement at the
+# top of each CHAI file. We use this to distinguish CHAI files from ones
+# that we do not own (e.g., other repos included as submodules), which we
+# do not want to modify. Note that this file and *.git files are omitted
 # as well.
 #
 # IMPORTANT: Since this file is not modified (it is running the shell 
 # script commands), you must EDIT THE COPYRIGHT DATES IN THE HEADER ABOVE 
 # MANUALLY.
 #
-# Edit the 'find' command below to change the set of files that will be
+# Edit the 'grep' command below to change the set of files that will be
 # modified.
 #
 # Change the 'sed' command below to change the content that is changed
-# in each file and what it is changed to.
+# in each file and what it is changed to. Typically, this would only be
+# modifying the end year in the copyright date.
 #
 #=============================================================================
 #
@@ -37,7 +38,7 @@
 #=============================================================================
 # First find all the files we want to modify
 #=============================================================================
-grep -rl "the CHAI LICENSE file" . --exclude-dir=.git --exclude-dir=blt --exclude-dir=umpire --exclude-dir=raja --exclude-dir=radiuss-spack-configs --exclude-dir=uberenv > files2change
+grep -rl "the CHAI LICENSE file" . --exclude-dir=.git --exclude-dir=blt --exclude-dir=umpire --exclude-dir=raja --exclude-dir=radiuss-spack-configs --exclude-dir=uberenv --exclude=update-copyright.sh > files2change
 
 #=============================================================================
 # Replace the old copyright dates with new dates
