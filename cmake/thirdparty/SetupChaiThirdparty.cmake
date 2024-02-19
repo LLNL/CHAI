a6facd005c92a2954f9f617c9b4a6c4340cc3482
Alan Dayton <6393677+adayton1@users.noreply.github.com>
2024-02-19 11:03:16 -0800

    Clean up CMake in CHAI (#247)

--- cmake/thirdparty/SetupChaiThirdparty.cmake
+++ cmake/thirdparty/SetupChaiThirdparty.cmake
@@ -4,67 +4,39 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 ##############################################################################
+
 if (NOT TARGET umpire)
-  if (DEFINED umpire_DIR)
-    # this allows umpire_DIR to be the install prefix if we are relying on umpire's
-    # installed umpire-config.cmake
-    list(APPEND CMAKE_PREFIX_PATH ${umpire_DIR})
-    find_package(umpire REQUIRED)
-
-    if (ENABLE_MPI)
-      set(UMPIRE_DEPENDS mpi)
-    else()
-      set(UMPIRE_DEPENDS)
-    endif()
-
-    blt_register_library(
-      NAME umpire
-      INCLUDES ${UMPIRE_INCLUDE_DIRS}
-      LIBRARIES umpire
-      DEPENDS_ON ${UMPIRE_DEPENDS})
+  if (DEFINED umpire_DIR OR DEFINED UMPIRE_DIR)
+    message(STATUS "[CHAI] Using external Umpire")
+    find_package(umpire CONFIG REQUIRED NO_DEFAULT_PATH PATHS ${umpire_DIR} ${UMPIRE_DIR})
   else ()
-    set(UMPIRE_ENABLE_FORTRAN Off CACHE BOOL "Enable Fortran in Umpire")
-    set(UMPIRE_ENABLE_C Off CACHE BOOL "Enable Fortran in Umpire")
-    set(UMPIRE_ENABLE_TESTS Off CACHE BOOL "")
-    set(UMPIRE_ENABLE_TOOLS Off CACHE BOOL "")
-    add_subdirectory(${PROJECT_SOURCE_DIR}/src/tpl/umpire)
-  endif()
-
-  # Umpire depends on camp
-  if (NOT TARGET camp)
-    if (DEFINED camp_DIR)
-      find_package(camp REQUIRED)
-      set_target_properties(camp PROPERTIES IMPORTED_GLOBAL TRUE)
+    if (NOT EXISTS ${PROJECT_SOURCE_DIR}/src/tpl/umpire/CMakeLists.txt)
+      message(FATAL_ERROR "[CHAI] Umpire not found! Set umpire_DIR to the install location of Umpire or run 'git submodule update --init --recursive' in the CHAI repository, then try building again.")
     else ()
-      message(FATAL_ERROR "camp is required. Please set camp_DIR")
-    endif()
+      message(STATUS "[CHAI] Using internal Umpire")
 
-    if(ENABLE_CUDA)
-      blt_add_target_definitions(
-        TO camp
-        SCOPE INTERFACE
-        TARGET_DEFINITIONS CAMP_HAVE_CUDA)
-    endif()
+      set(UMPIRE_ENABLE_FORTRAN Off CACHE BOOL "Enable Fortran in Umpire")
+      set(UMPIRE_ENABLE_C Off CACHE BOOL "Enable Fortran in Umpire")
+      set(UMPIRE_ENABLE_TESTS Off CACHE BOOL "Enable tests in Umpire")
+      set(UMPIRE_ENABLE_TOOLS Off CACHE BOOL "Enable tools in Umpire")
+
+      add_subdirectory(${PROJECT_SOURCE_DIR}/src/tpl/umpire)
+    endif ()
   endif ()
-endif()
+endif ()
 
 if (CHAI_ENABLE_RAJA_PLUGIN)
   if (NOT TARGET RAJA)
-    if (DEFINED RAJA_DIR)
-      # this allows RAJA_DIR to be the install prefix if we are relying on RAJA's
-      # installed RAJA-config.cmake
-      list(APPEND CMAKE_PREFIX_PATH ${RAJA_DIR})
-      message(STATUS "CHAI: using external RAJA via find_package ${RAJA_DIR}")
-      find_package(RAJA REQUIRED)
-       blt_register_library(
-         NAME RAJA
-         INCLUDES ${RAJA_INCLUDE_DIRS}
-         LIBRARIES RAJA)
-    else()
-      message(STATUS "CHAI: using builtin RAJA submodule")
-      add_subdirectory(${PROJECT_SOURCE_DIR}/src/tpl/raja)
-    endif()
-  else()
-    message(STATUS "CHAI: using existing RAJA target")
-  endif()
+    if (DEFINED raja_DIR OR DEFINED RAJA_DIR)
+      message(STATUS "[CHAI] Using external RAJA")
+      find_package(raja CONFIG REQUIRED NO_DEFAULT_PATH PATHS ${raja_DIR} ${RAJA_DIR})
+    else ()
+      if (NOT EXISTS ${PROJECT_SOURCE_DIR}/src/tpl/raja/CMakeLists.txt)
+        message(FATAL_ERROR "[CHAI] RAJA not found! Set raja_DIR to the install location of RAJA or run 'git submodule update --init --recursive' in the CHAI repository, then try building again.")
+      else ()
+        message(STATUS "[CHAI] Using internal RAJA")
+        add_subdirectory(${PROJECT_SOURCE_DIR}/src/tpl/raja)
+      endif ()
+    endif ()
+  endif ()
 endif ()
