2f2eef6683c1f622048ca70f3cad85c21c950f21
Alan Dayton <dayton8@llnl.gov>
2024-08-09 15:49:45 -0700

    Update host config

--- host-configs/lc/toss_4_x86_64_ib_cray/amdclang.cmake
+++ host-configs/lc/toss_4_x86_64_ib_cray/amdclang.cmake
@@ -8,6 +8,7 @@
 set(ENABLE_HIP ON CACHE BOOL "Enable Hip")
 set(ROCM_PATH "/usr/tce/packages/rocmcc/rocmcc-6.2.0-magic" CACHE PATH "")
 set(CMAKE_HIP_ARCHITECTURES "gfx942:xnack+" CACHE STRING "")
+set(AMDGPU_TARGETS "gfx942:xnack+" CACHE STRING "")
 
 set(CMAKE_C_COMPILER "${ROCM_PATH}/bin/amdclang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "${ROCM_PATH}/bin/amdclang++" CACHE PATH "")
