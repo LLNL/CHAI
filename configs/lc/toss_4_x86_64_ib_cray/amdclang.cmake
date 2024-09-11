df271fb445641d0171f154585e057799a4c32224
Benjamin T. Liu <liu15@llnl.gov>
2024-09-11 16:02:26 -0700

    Tweak build

--- configs/lc/toss_4_x86_64_ib_cray/amdclang.cmake
+++ configs/lc/toss_4_x86_64_ib_cray/amdclang.cmake
@@ -5,12 +5,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 ##############################################################################
 
-set(COMPILER_BASE "/usr/tce/packages/rocmcc/rocmcc-6.1.0-magic" CACHE PATH "")
+set(COMPILER_BASE "/usr/tce/packages/rocmcc/rocmcc-6.2.0-magic" CACHE PATH "")
 set(CMAKE_C_COMPILER "${COMPILER_BASE}/bin/amdclang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "${COMPILER_BASE}/bin/amdclang++" CACHE PATH "")
 
 set(ENABLE_HIP ON CACHE BOOL "Enable Hip")
-set(ROCM_PATH "/usr/tce/packages/rocmcc/rocmcc-6.1.0-magic" CACHE PATH "")
+set(ROCM_PATH "/usr/tce/packages/rocmcc/rocmcc-6.2.0-magic" CACHE PATH "")
 set(CMAKE_HIP_ARCHITECTURES "gfx942:xnack+" CACHE STRING "")
 set(AMDGPU_TARGETS "gfx942:xnack+" CACHE STRING "")
 
