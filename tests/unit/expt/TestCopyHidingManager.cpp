d72a12c4f821fc56f1bdc383f3f5c0023099ac15
Alan Dayton <dayton8@llnl.gov>
2025-05-20 15:07:22 -0700

    Update tests

--- tests/unit/expt/TestCopyHidingManager.cpp
+++ tests/unit/expt/TestCopyHidingManager.cpp
@@ -10,7 +10,18 @@
 #include "chai/expt/CopyHidingManager.hpp"
 #include "umpire/ResourceManager.hpp"
 
-class CopyHidingManagerTest : public testing::Test {
+/*!
+ * CopyHidingManager has many states and transitions. A test fixture is created
+ * for each state, and a test case is created for each transition.
+ */
+
+/*!
+ * \class CopyHidingManager_StateBothUnallocated_Test
+ *
+ * \brief Test fixture for the state where both host and device data are
+ *        unallocated (and untouched)
+ */
+class CopyHidingManager_StateBothUnallocated_Test : public testing::Test {
   protected:
     int m_host_allocator_id{umpire::ResourceManager::getInstance().getAllocator("HOST").getId()};
     int m_device_allocator_id{umpire::ResourceManager::getInstance().getAllocator("DEVICE").getId()};
@@ -20,59 +31,58 @@ class CopyHidingManagerTest : public testing::Test {
                                             m_size};
 };
 
-
-TEST_F(CopyHidingManagerTest, HostAllocatorID)
-{
-  EXPECT_EQ(m_host_allocator_id, m_manager.getHostAllocatorID());
-}
-
-TEST_F(CopyHidingManagerTest, DeviceAllocatorID)
-{
-  EXPECT_EQ(m_device_allocator_id, m_manager.getDeviceAllocatorID());
-}
-
-TEST_F(CopyHidingManagerTest, Size)
+TEST_F(CopyHidingManager_StateBothUnallocated_Test, Size)
 {
   EXPECT_EQ(m_size, m_manager.size());
 }
 
-TEST_F(CopyHidingManagerTest, DataExecutionContextNone)
+TEST_F(CopyHidingManager_StateBothUnallocated_Test, HostAllocatorID)
+{
+  EXPECT_EQ(m_host_allocator_id, m_manager.getHostAllocatorID());
+}
+
+TEST_F(CopyHidingManager_StateBothUnallocated_Test, DeviceAllocatorID)
+{
+  EXPECT_EQ(m_device_allocator_id, m_manager.getDeviceAllocatorID());
+}
+
+TEST_F(CopyHidingManager_StateBothUnallocated_Test, Touch)
+{
+  EXPECT_EQ(chai::expt::ExecutionContext::NONE, m_manager.getTouch());
+}
+
+TEST_F(CopyHidingManager_StateBothUnallocated_Test, Data_None)
 {
   EXPECT_EQ(nullptr, m_manager.data(chai::expt::ExecutionContext::NONE, false));
   EXPECT_EQ(chai::expt::ExecutionContext::NONE, m_manager.getTouch());
 }
 
-TEST_F(CopyHidingManagerTest, DataExecutionContextHost)
-{
-  EXPECT_NE(nullptr, m_manager.data(chai::expt::ExecutionContext::HOST, false));
-  EXPECT_EQ(chai::expt::ExecutionContext::NONE, m_manager.getTouch());
-}
-
-TEST_F(CopyHidingManagerTest, DataExecutionContextDevice)
-{
-  EXPECT_NE(nullptr, m_manager.data(chai::expt::ExecutionContext::DEVICE, false));
-  EXPECT_EQ(chai::expt::ExecutionContext::NONE, m_manager.getTouch());
-}
-
-TEST_F(CopyHidingManagerTest, Touch)
-{
-  EXPECT_EQ(chai::expt::ExecutionContext::NONE, m_manager.getTouch());
-}
-
-TEST_F(CopyHidingManagerTest, DataExecutionContextNoneTouch)
+TEST_F(CopyHidingManager_StateBothUnallocated_Test, Data_None_Touch)
 {
   EXPECT_EQ(nullptr, m_manager.data(chai::expt::ExecutionContext::NONE, true));
   EXPECT_EQ(chai::expt::ExecutionContext::NONE, m_manager.getTouch());
 }
 
-TEST_F(CopyHidingManagerTest, DataExecutionContextHostTouch)
+TEST_F(CopyHidingManager_StateBothUnallocated_Test, Data_Host)
+{
+  EXPECT_NE(nullptr, m_manager.data(chai::expt::ExecutionContext::HOST, false));
+  EXPECT_EQ(chai::expt::ExecutionContext::NONE, m_manager.getTouch());
+}
+
+TEST_F(CopyHidingManager_StateBothUnallocated_Test, Data_Host_Touch)
 {
   EXPECT_NE(nullptr, m_manager.data(chai::expt::ExecutionContext::HOST, true));
   EXPECT_EQ(chai::expt::ExecutionContext::HOST, m_manager.getTouch());
 }
 
-TEST_F(CopyHidingManagerTest, DataExecutionContextDeviceTouch)
+TEST_F(CopyHidingManager_StateBothUnallocated_Test, Data_Device)
 {
-  EXPECT_NE(nullptr, m_manager.data(chai::expt::ExecutionContext::DEVICE, true));
+  EXPECT_NE(nullptr, m_manager.data(chai::expt::ExecutionContext::DEVICE, false));
+  EXPECT_EQ(chai::expt::ExecutionContext::NONE, m_manager.getTouch());
+}
+
+TEST_F(CopyHidingManager_StateBothUnallocated_Test, Data_Device_Touch)
+{
+  EXPECT_NE(nullptr, m_manager.data(chai::expt::ExecutionContext::DEVICE, false));
   EXPECT_EQ(chai::expt::ExecutionContext::DEVICE, m_manager.getTouch());
 }
