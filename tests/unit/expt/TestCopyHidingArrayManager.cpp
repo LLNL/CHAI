46d7f40670a8f2f12e83c170e4c6b93d85a2e803
Alan Dayton <dayton8@llnl.gov>
2025-08-12 11:09:51 -0700

    Add test file for CopyHidingArrayManager

---
+++ tests/unit/expt/TestCopyHidingArrayManager.cpp
@@ -0,0 +1,11 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-25, Lawrence Livermore National Security, LLC and CHAI
+// project contributors. See the CHAI LICENSE file for details.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//////////////////////////////////////////////////////////////////////////////
+#include "gtest/gtest.h"
+
+#include "chai/config.hpp"
+#include "chai/expt/CopyHidingManager.hpp"
+#include "umpire/ResourceManager.hpp"
