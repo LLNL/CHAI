5387d312876b52a8a9cce2f935ddfc93e48abee5
Alan Dayton <dayton8@llnl.gov>
2025-08-29 17:01:37 -0700

    Rename types

--- src/chai/expt/UnifiedMemoryArray.hpp
+++ src/chai/expt/UnifiedMemoryArray.hpp
@@ -1,300 +1,200 @@
 #ifndef CHAI_UNIFIED_MEMORY_ARRAY_HPP
 #define CHAI_UNIFIED_MEMORY_ARRAY_HPP
 
-#include "chai/expt/ArrayManager.hpp"
-#include "chai/expt/ExecutionContextManager.hpp"
-
-#include <cstddef>
-#include <stdexcept>
+#include "chai/expt/ExecutionContext.hpp"
+#include "umpire/ResourceManager.hpp"
 
 namespace chai {
 namespace expt {
   /*!
    * \class UnifiedMemoryArray
    *
-   * \brief A data structure for managing the lifetime and coherence of a
+   * \brief A container for managing the lifetime and coherence of a
    *        unified memory array, meaning an array with a single address
    *        that is accessible from all processors/devices in a system.
    *
-   * This data structure is designed for use with the ExecutionContextManager.
-   * When the execution context is set and the copy constructor is triggered,
-   * the underlying data is made coherent in the current execution context.
-   *
-   * This lends itself particularly well to an elegant programming model
-   * where loop bodies are replaced with lambda expressions that capture
-   * variables in the current scope by copy. If the execution context has
-   * already been set, the lambda expression can be executed in that context.
-   * Otherwise, the execution context must be set and then a copy of the
-   * lambda expression (which triggers a copy of all the captured variables)
-   * can be executed in that context. This latter case is how CHAI works
-   * with RAJA via a RAJA plugin.
+   * This container should be used in tandem with the ExecutionContextManager.
+   * Together, they provide a programming model where work (e.g. a kernel)
+   * is generally performed asynchronously, with synchronization occurring
+   * only as needed for coherence of the array. For example, if the array is
+   * written to in an asynchronize kernel on a GPU, then the GPU will be
+   * synchronized if the array needs to be accessed on the CPU.
    *
    * This model works well for APUs where the CPU and GPU have the same
-   * physical memory. It also works for pinned (i.e. page-locked) memory.
+   * physical memory. It also works for pinned (i.e. page-locked) memory
+   * and in some cases for pageable memory, though no pre-fetching is
+   * performed.
    *
-   * Example using RAJA and CHAI:
+   * Example:
    *
    * \code
-   * #include "chai/UnifiedMemoryArray.hpp"
-   * #include "RAJA/RAJA.hpp"
-   *
-   * constexpr int CUDA_BLOCK_SIZE = 256;
-   * constexpr int ASYNCHRONOUS = true;
-   *
+   * // Create a UnifiedMemoryArray with size 100 and default allocator
    * int size = 10000;
-   * chai::UnifiedMemoryArray<int> a(size);
+   * UnifiedMemoryArray<int> array(size);
    *
-   * int offset = 42;
+   * // Access elements on the device
+   * std::span<int> device_view(array.data(ExecutionContext::DEVICE, array.size());
    *
-   * // Both `a` and `offset` are captured by copy into the lambda expression.
-   * // The execution context is not set, so the copy constructor of `a`
-   * // results in a shallow copy with nothing done related to coherence
-   * // management. RAJA then calls the CHAI plugin, which sets the current
-   * // execution context. At that point, the lambda expression is then copied,
-   * // which again triggers the copy constructor of `a`. This time, the data
-   * // is made coherent on the device (which is essentially a no-op because it
-   * // has not been accessed in any other execution context yet). After this,
-   * // RAJA calls the CHAI plugin to reset the execution context and launches
-   * // a CUDA kernel that executes the lambda expression for each index in
-   * // [0, size).
-   * RAJA::forall<RAJA::cuda_exec<CUDA_BLOCK_SIZE, ASYNCHRONOUS>>(RAJA::TypedRangeSegment<int>(0, size), [=] __device__ (int i) {
-   *   a[i] = offset + i; 
-   * });
+   * // Launch a kernel that modifies device_view.
+   * // Note that this example relies on c++20 and the ability to use constexpr
+   * // host code on the device.
    *
-   * // The same process as described above happens, except that now `a` is
-   * // made coherent on the host by synchronizing the device and the lambda
-   * // expression is evaluated on the host.
-   * RAJA::forall<RAJA::seq_exec>(RAJA::TypedRangeSegment<int>(0, size), [=] __device__ (int i) {
-   *   a[i] = i - offset;
-   * });
+   * // Access elements on the host. This will synchronize the device.
+   * std::span<int> host_view(array.data(ExecutionContext::HOST), array.size());
+   *
+   * for (int i = 0; i < size; ++i) {
+   *   std::cout << host_view[i] << "\n";
+   * }
+   *
+   * // Access and modify individual elements in the container.
+   * // This should be used sparingly or it will tank performance.
+   * // Getting the last element after performing a scan is one use case.
+   * array.get(ExecutionContext::HOST, size - 1) = 10;
    * \endcode
    */
   template <typename T>
-  class UnifiedMemoryArray {
+  class UnifiedMemoryArray
     public:
-      /*!
-       * \brief Constructs an empty array.
-       */
-      constexpr UnifiedMemoryArray() noexcept = default;
+      UnifiedMemoryArray() = default;
 
-      explicit UnifiedMemoryArray(const umpire::Allocator& allocator)
-        : m_manager{new UnifiedMemoryManager(allocator)}
+      explicit UnifiedMemoryArray(const umpire::Allocator& allocator) :
+        m_allocator{allocator}
       {
       }
 
-      UnifiedMemoryArray(std::size_t size, const umpire::Allocator& allocator)
-        : m_size{size},
-          m_manager{new UnifiedMemoryManager(size * sizeof(T), allocator)}
+      UnifiedMemoryArray(std::size_t size, const umpire::Allocator& allocator) :
+        m_size{size},
+        m_allocator{allocator}
+      {
+        m_data = m_allocator.allocate(m_size * sizeof(T));
+        // TODO: Investigate if/when to do initialization
+      }
+
+      explicit UnifiedMemoryArray(int allocatorID) :
+        m_allocator{umpire::ResourceManager::getInstance().getAllocator(allocatorID)}
       {
       }
 
-      explicit UnifiedMemoryArray(int allocatorID)
-        : m_manager{new UnifiedMemoryManager(allocatorID)}
+      UnifiedMemoryArray(std::size_t size, int allocatorID) :
+        m_size{size},
+        m_allocator{umpire::ResourceManager::getInstance().getAllocator(allocatorID)}
       {
+        m_data = m_allocator.allocate(m_size * sizeof(T));
+        // TODO: Investigate if/when to do initialization
       }
 
-      UnifiedMemoryArray(std::size_t size, int allocatorID)
-        : m_size{size},
-          m_manager{new UnifiedMemoryManager(size * sizeof(T), allocatorID)}
+      UnifiedMemoryArray(const UnifiedMemoryArray& other) :
+        m_size{other.m_size},
+        m_allocator{other.m_allocator}
       {
+        m_data = m_allocator.allocate(m_size * sizeof(T));
+        ExecutionContextManager::getInstance().setExecutionContext(ExecutionContext::DEVICE);
+        umpire::ResourceManager::getInstance().copy(other.m_data, m_data, m_size * sizeof(T));
+        ExecutionContextManager::getInstance().setExecutionContext(ExecutionContext::NONE);
+        m_last_execution_context = ExecutionContext::DEVICE;
       }
 
-      explicit UnifiedMemoryArray(UnifiedMemoryManager* manager)
-        : m_manager{manager}
-      {
-        if (m_manager)
-        {
-          m_size = m_manager->size() / sizeof(T);
-        }
-      }
-
-      /*!
-       * \brief Constructs a shallow copy of an array from another and makes
-       *        the data coherent in the current execution space.
-       *
-       * \param other The other array.
-       *
-       * \note This is a shallow copy.
-       */
-      CHAI_HOST_DEVICE UnifiedMemoryArray(const UnifiedMemoryArray& other) :
+      UnifiedMemoryArray(UnifiedMemoryArray&& other) :
         m_data{other.m_data},
-        m_size{other.m_size}
-#if !defined(CHAI_DEVICE_COMPILE)
-        , m_manager{other.m_manager}
-#endif
+        m_size{other.m_size},
+        m_last_execution_context{other.m_last_execution_context},
+        m_allocator{other.m_allocator}
       {
-        update();
+        other.m_data = nullptr;
+        other.m_size = 0;
+        other.m_last_execution_context = NONE;
+        other.m_allocator = umpire::Allocator();
       }
 
-      /*!
-       * \brief Sets the array manager for this UnifiedMemoryArray.
-       *
-       * \param manager The new array manager to be set.
-       *
-       * \post The UnifiedMemoryArray takes ownership of the new manager objet.
-       */
-      void setManager(UnifiedMemoryManager* manager)
-      {
-        delete m_manager;
-        m_manager = manager;
+      UnifiedMemoryArray& operator=(const UnifiedMemoryArray& other) {
+        if (&other != this) { // Prevent self-assignment
+          m_allocator.deallocate(m_data);
 
-        if (m_manager)
-        {
-          m_size = m_manager->size() / sizeof(T);
+          m_size = other.m_size;
+          m_allocator = other.m_allocator;
+          m_data = m_allocator.allocate(m_size * sizeof(T));
+          ExecutionContextManager::getInstance().setExecutionContext(ExecutionContext::DEVICE);
+          umpire::ResourceManager::getInstance().copy(other.m_data, m_data, m_size * sizeof(T));
+          ExecutionContextManager::getInstance().setExecutionContext(ExecutionContext::NONE);
+          m_last_execution_context = ExecutionContext::DEVICE;
         }
+
+        return *this;
       }
 
-      /*!
-       * \brief Get the array manager associated with this UnifiedMemoryArray.
-       *
-       * \return A pointer to the array manager.
-       */
-      UnifiedMemoryManager* getManager() const
-      {
-        return m_manager;
-      }
+      UnifiedMemoryArray& operator=(UnifiedMemoryArray&& other) {
+        if (&other != this) {
+          m_allocator.deallocate(m_data);
 
-      /*!
-       * \brief Resizes the array to the specified new size.
-       *
-       * \param newSize The new size to resize the array to.
-       *
-       * \note This method updates the size of the array and triggers a resize operation in the array manager if it exists.
-       *       If no array manager is associated, an exception is thrown.
-       */
-      void resize(std::size_t newSize)
-      {
-        if (m_manager) {
-          m_size = newSize;
-          m_manager->resize(newSize * sizeof(T));
+          m_data = other.m_data;
+          m_size = other.m_size;
+          m_last_execution_context = other.m_last_execution_context;
+          m_allocator = other.m_allocator;
+          
+          other.m_data = nullptr;
+          other.m_size = 0;
+          other.m_last_execution_context = ExecutionContext::NONE;
+          other.m_allocator = umpire::Allocator();
         }
-        else {
-          m_manager = new UnifiedMemoryManager(newSize * sizeof(T));
-        }
+
+        return *this;
       }
 
       /*!
-       * \brief Frees the resources associated with this array.
-       *
-       * \note Once free has been called, it is invalid to use any other copies
-       *       of this array (since copies are shallow).
+       * \brief Destructor.
        */
-      void free() {
-        m_data = nullptr;
-        m_size = 0;
-        delete m_manager;
-        m_manager = nullptr;
+      ~UnifiedMemoryArray() {
+        m_allocator.deallocate(m_data);
       }
 
       /*!
-       * \brief Get the number of elements in the array.
-       *
-       * \pre The copy constructor has been called with the execution space
-       *      set to CPU or GPU (e.g. by the RAJA plugin).
+       * \brief Get the number of elements.
        */
-      CHAI_HOST_DEVICE std::size_t size() const
-      {
+      size_t size() const {
         return m_size;
       }
 
-      CHAI_HOST_DEVICE update() const
-      {
-#if !defined(CHAI_DEVICE_COMPILE)
-        if (m_manager) {
-          m_data = static_cast<T*>(m_manager->data(!std::is_const<T>::value));
-          m_size = m_manager->size() / sizeof(T);
+      T* data(ExecutionContext executionContext) {
+        if (executionContext != m_last_execution_context) {
+          ExecutionContextManager::getInstance().synchronize(m_last_execution_context);
+          m_last_execution_context = executionContext;
         }
-#endif
-      }
 
-      /*!
-       * \brief Get a pointer to the element data in the current execution context.
-       *
-       * \return A pointer to the element data in the current execution context.
-       */
-      CHAI_HOST_DEVICE T* data() const
-      {
-        update();
         return m_data;
       }
 
-      /*!
-       * \brief Get a const pointer to the element data in the current execution context.
-       *
-       * \return A const pointer to the element data in the current execution context.
-       */
-      CHAI_HOST_DEVICE const T* cdata() const {
-#if !defined(CHAI_DEVICE_COMPILE)
-        if (m_manager) {
-          m_data = static_cast<T*>(m_manager->data(false));
-          m_size = m_manager->size() / sizeof(T);
+      const T* data(ExecutionContext executionContext) const {
+        if (executionContext != m_last_execution_context) {
+          ExecutionContextManager::getInstance().synchronize(m_last_execution_context);
+          m_last_execution_context = ExecutionContext::NONE;
         }
-#endif
+
         return m_data;
       }
 
-      /*!
-       * \brief Get the ith element in the array.
-       *
-       * \param i The index of the element to retrieve.
-       *
-       * \pre The copy constructor has been called with the execution space
-       *      set to CPU or GPU (e.g. by the RAJA plugin).
-       */
-      CHAI_HOST_DEVICE T& operator[](std::size_t i) const {
+      T& get(ExecutionContext executionContext, size_t i) {
+        if (executionContext != m_last_execution_context) {
+          ExecutionContextManager::getInstance().synchronize(m_last_execution_context);
+          m_last_execution_context = executionContext;
+        }
+
         return m_data[i];
       }
 
-      /*!
-       * \brief Get the value of the element at the specified index.
-       *
-       * \param i The index of the element to retrieve.
-       *
-       * \return The value of the element at the specified index.
-       *
-       * \throw std::runtime_exception if unable to retrieve the element.
-       */
-      T get(std::size_t i) const {
-        if (m_manager) {
-          return m_manager->get(i);
+      const T& get(ExecutionContext executionContext, size_t i) {
+        if (executionContext != m_last_execution_context) {
+          ExecutionContextManager::getInstance().synchronize(m_last_execution_context);
+          m_last_execution_context = ExecutionContext::NONE;
         }
-        else {
-          throw std::out_of_range();
-        }
-      }
 
-      /*!
-       * \brief Set a value at a specified index in the array.
-       *
-       * \param i The index where the value is to be set.
-       * \param value The value to set at the specified index.
-       *
-       * \throw std::runtime_exception if the array manager is not associated with the UnifiedMemoryArray.
-       */
-      void set(std::size_t i, const T& value) {
-        if (m_manager) {
-          m_manager->set(i, value);
-        }
-        else {
-          throw std::out_of_range();
-        }
+        return m_data[i];
       }
 
     private:
-      /*!
-       * The array that is coherent in the current execution space.
-       */
       T* m_data{nullptr};
-
-      /*!
-       * The number of elements in the array.
-       */
-      std::size_t m_size{0};
-
-      /*!
-       * The array manager controls the coherence of the array.
-       */
-      UnifiedMemoryManager* m_manager{nullptr};
+      size_t m_size{0};
+      ExecutionContext m_last_execution_context{ExecutionContext::NONE};
+      umpire::Allocator m_allocator{};
   };  // class UnifiedMemoryArray
 }  // namespace expt
 }  // namespace chai
