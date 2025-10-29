fb1940b04a613208c1570df56028abde524c8e2e
Alan Dayton <dayton8@llnl.gov>
2025-10-28 17:02:34 -0700

    Add HostDeviceArrayManager and tests

---
+++ src/chai/expt/HostDeviceArrayManager.hpp
@@ -0,0 +1,345 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-25, Lawrence Livermore National Security, LLC and CHAI
+// project contributors. See the CHAI LICENSE file for details.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//////////////////////////////////////////////////////////////////////////////
+#ifndef CHAI_HOST_DEVICE_ARRAY_MANAGER_HPP
+#define CHAI_HOST_DEVICE_ARRAY_MANAGER_HPP
+
+#include "chai/expt/Context.hpp"
+#include "chai/expt/ContextManager.hpp"
+#include "umpire/Allocator.hpp"
+#include "umpire/ResourceManager.hpp"
+#include <cstddef>
+
+namespace chai::expt
+{
+  template <typename T>
+  class HostDeviceArrayManager {
+    public:
+      HostDeviceArrayManager() = default;
+
+      HostDeviceArrayManager(const umpire::Allocator& host_allocator,
+                             const umpire::Allocator& device_allocator)
+        : m_host_allocator{host_allocator},
+          m_device_allocator{device_allocator}
+      {
+      }
+
+      explicit HostDeviceArrayManager(std::size_t size)
+      {
+        resize(size);
+      }
+
+      HostDeviceArrayManager(std::size_t size,
+                             const umpire::Allocator& host_allocator,
+                             const umpire::Allocator& device_allocator)
+        : m_host_allocator{host_allocator},
+          m_device_allocator{device_allocator}
+      {
+        resize(size);
+      }
+
+      HostDeviceArrayManager(const HostDeviceArrayManager& other)
+        : m_host_allocator{other.m_host_allocator},
+          m_device_allocator{other.m_device_allocator}
+      {
+        resize(other.m_size);
+        umpire::ResourceManager::getInstance().copy(other.m_device_data, m_device_data, m_size * sizeof(T));
+      }
+
+      HostDeviceArrayManager(HostDeviceArrayManager&& other)
+        : m_host_data{other.m_host_data},
+          m_device_data{other.m_device_data},
+          m_size{other.m_size},
+          m_modified{other.m_modified},
+          m_host_allocator{other.m_host_allocator},
+          m_device_allocator{other.m_device_allocator}
+      {
+        other.m_host_data = nullptr;
+        other.m_device_data = nullptr;
+        other.m_size = 0;
+        other.m_modified = Context::NONE;
+      }
+
+      ~HostDeviceArrayManager()
+      {
+        m_host_allocator.deallocate(m_host_data);
+        m_device_allocator.deallocate(m_device_data);
+      }
+
+      HostDeviceArrayManager& operator=(const HostDeviceArrayManager& other)
+      {
+        if (&other != this)
+        {
+          m_host_allocator.deallocate(m_host_data);
+          m_host_data = nullptr;
+
+          m_device_allocator.deallocate(m_device_data);
+          m_device_data = nullptr;
+
+          m_size = 0;
+
+          m_host_allocator = other.m_host_allocator;
+          m_device_allocator = other.m_device_allocator;
+
+          resize(other.m_size);
+          // TODO: Fix the copy
+          umpire::ResourceManager::getInstance().copy(other.m_device_data, m_device_data, m_size * sizeof(T));
+        }
+
+        return *this;
+      }
+
+      HostDeviceArrayManager& operator=(HostDeviceArrayManager&& other)
+      {
+        if (&other != this)
+        {
+          m_host_allocator.deallocate(m_host_data);
+          m_device_allocator.deallocate(m_device_data);
+
+          m_host_data = other.m_host_data;
+          m_device_data = other.m_device_data;
+          m_size = other.m_size;
+          m_modified = other.m_modified;
+          m_host_allocator = other.m_host_allocator;
+          m_device_allocator = other.m_device_allocator;
+
+          other.m_host_data = nullptr;
+          other.m_device_data = nullptr;
+          other.m_size = 0;
+          other.m_modified = Context::NONE;
+        }
+
+        return *this;
+      }
+
+      void resize(std::size_t new_size)
+      {
+        if (new_size != m_size)
+        {
+          std::size_t old_size_bytes = m_size   * sizeof(T);
+          std::size_t new_size_bytes = new_size * sizeof(T);
+
+          if (m_modified == Context::HOST ||
+              (m_host_data && !m_device_data))
+          {
+            if (m_device_data)
+            {
+              m_device_allocator.deallocate(m_device_data);
+              m_device_data = nullptr;
+            }
+
+            T* new_host_data = nullptr;
+
+            if (new_size > 0)
+            {
+              new_host_data = static_cast<T*>(m_host_allocator.allocate(new_size_bytes));
+            }
+
+            if (m_host_data)
+            {
+              umpire::ResourceManager::getInstance().copy(m_host_data, new_host_data, std::min(old_size_bytes, new_size_bytes));
+              m_host_allocator.deallocate(m_host_data);
+            }
+
+            m_host_data = new_host_data;
+          }
+          else
+          {
+            if (m_host_data)
+            {
+              m_host_allocator.deallocate(m_host_data);
+              m_host_data = nullptr;
+            }
+
+            T* new_device_data = nullptr;
+
+            if (new_size > 0)
+            {
+              new_device_data = static_cast<T*>(m_device_allocator.allocate(new_size_bytes));
+            }
+
+            if (m_device_data)
+            {
+              umpire::ResourceManager::getInstance().copy(m_device_data, new_device_data, std::min(old_size_bytes, new_size_bytes));
+              m_device_allocator.deallocate(m_device_data);
+            }
+
+            m_device_data = new_device_data;
+          }
+
+          m_size = new_size;
+        }
+      }
+
+      void free()
+      {
+        m_host_allocator.deallocate(m_host_data);
+        m_host_data = nullptr;
+
+        m_device_allocator.deallocate(m_device_data);
+        m_device_data = nullptr;
+
+        m_size = 0;
+        m_modified = Context::NONE;
+      }
+
+      std::size_t size() const
+      {
+        return m_size;
+      }
+
+      T* data()
+      {
+        Context context =
+          ContextManager::getInstance().getContext();
+
+        if (context == Context::DEVICE)
+        {
+          if (m_device_data == nullptr)
+          {
+            m_device_data = static_cast<T*>(m_device_allocator.allocate(m_size * sizeof(T)));
+          }
+
+          if (m_modified == Context::HOST)
+          {
+            umpire::ResourceManager::getInstance().copy(m_host_data, m_device_data, m_size * sizeof(T));
+          }
+
+          m_modified = Context::DEVICE;
+          return m_device_data;
+        }
+        else if (context == Context::HOST)
+        {
+          if (m_host_data == nullptr)
+          {
+            m_host_data = static_cast<T*>(m_host_allocator.allocate(m_size * sizeof(T)));
+          }
+
+          if (m_modified == Context::DEVICE)
+          {
+            umpire::ResourceManager::getInstance().copy(m_device_data, m_host_data, m_size * sizeof(T));
+          }
+
+          m_modified = Context::HOST;
+          return m_host_data;
+        }
+        else
+        {
+          return nullptr;
+        }
+      }
+
+      const T* data() const
+      {
+        Context context =
+          ContextManager::getInstance().getContext();
+
+        if (context == Context::DEVICE)
+        {
+          if (m_device_data == nullptr)
+          {
+            m_device_data = static_cast<T*>(m_device_allocator.allocate(m_size * sizeof(T)));
+          }
+
+          if (m_modified == Context::HOST)
+          {
+            umpire::ResourceManager::getInstance().copy(m_host_data, m_device_data, m_size * sizeof(T));
+            m_modified = Context::NONE;
+          }
+
+          return m_device_data;
+        }
+        else if (context == Context::HOST)
+        {
+          if (m_host_data == nullptr)
+          {
+            m_host_data = static_cast<T*>(m_host_allocator.allocate(m_size * sizeof(T)));
+          }
+
+          if (m_modified == Context::DEVICE)
+          {
+            umpire::ResourceManager::getInstance().copy(m_device_data, m_host_data, m_size * sizeof(T));
+            m_modified = Context::NONE;
+          }
+
+          return m_host_data;
+        }
+        else
+        {
+          return nullptr;
+        }
+      }
+
+      T get(std::size_t i) const
+      {
+        T result;
+
+        if (m_modified == Context::DEVICE)
+        {
+          umpire::ResourceManager::getInstance().copy(m_device_data + i, &result, sizeof(T));
+        }
+        else
+        {
+          result = m_host_data[i];
+        }
+
+        return result;
+      }
+
+      void set(std::size_t i, T value)
+      {
+        if (m_modified == Context::DEVICE)
+        {
+          umpire::ResourceManager::getInstance().copy(&value, m_device_data + i, sizeof(T));
+        }
+        else
+        {
+          if (m_host_data == nullptr)
+          {
+            m_host_data = static_cast<T*>(m_host_allocator.allocate(m_size * sizeof(T)));
+          }
+
+          m_host_data[i] = value;
+          m_modified = Context::HOST;
+        }
+      }
+
+      T* host_data()
+      {
+        return m_host_data;
+      }
+
+      T* device_data()
+      {
+        return m_device_data;
+      }
+
+      Context modified() const
+      {
+        return m_modified;
+      }
+
+      umpire::Allocator host_allocator() const
+      {
+        return m_host_allocator;
+      }
+
+      umpire::Allocator device_allocator() const
+      {
+        return m_device_allocator;
+      }
+
+    private:
+      mutable T* m_host_data{nullptr};
+      mutable T* m_device_data{nullptr};
+      std::size_t m_size{0};
+      mutable Context m_modified{Context::NONE};
+      mutable umpire::Allocator m_host_allocator{umpire::ResourceManager::getInstance().getAllocator("HOST")};
+      mutable umpire::Allocator m_device_allocator{umpire::ResourceManager::getInstance().getAllocator("DEVICE")};
+  };  // class HostDeviceArrayManager
+}  // namespace chai::expt
+
+#endif  // CHAI_HOST_DEVICE_ARRAY_MANAGER_HPP
---
+++ tests/expt/HostDeviceArrayManagerTests.cpp
@@ -0,0 +1,484 @@
+#include "chai/expt/HostDeviceArrayManager.hpp"
+#include "chai/expt/Context.hpp"
+#include "chai/expt/ContextManager.hpp"
+#include "umpire/Allocator.hpp"
+#include "umpire/ResourceManager.hpp"
+#include "umpire/strategy/QuickPool.hpp"
+#include "gtest/gtest.h"
+
+enum class ContextManagerState
+{
+  CONTEXT_NONE_SYNCHRONIZED_DEVICE,
+  CONTEXT_HOST_SYNCHRONIZED_DEVICE,
+  CONTEXT_DEVICE_SYNCHRONIZED_DEVICE,
+  CONTEXT_NONE_UNSYNCHRONIZED_DEVICE,
+  CONTEXT_HOST_UNSYNCHRONIZED_DEVICE,
+  CONTEXT_DEVICE_UNSYNCHRONIZED_DEVICE
+};
+
+enum class HostDeviceArrayManagerState
+{
+  ALLOCATED_NONE,
+  ALLOCATED_HOST,
+  ALLOCATED_DEVICE,
+  ALLOCATED_HOST_MODIFIED_HOST,
+  ALLOCATED_DEVICE_MODIFIED_DEVICE,
+  ALLOCATED_BOTH,
+  ALLOCATED_BOTH_MODIFIED_HOST,
+  ALLOCATED_BOTH_MODIFIED_DEVICE
+}
+
+class HostDeviceArrayManagerTest : public ::testing::Test
+{
+  protected:
+    static chai::expt::ContextManager& GetContextManager()
+    {
+      static chai::expt::ContextManager& s_context_manager =
+        chai::expt::ContextManager::getInstance();
+
+      return s_context_manager;
+    }
+
+    static umpire::ResourceManager& GetResourceManager()
+    {
+      static umpire::ResourceManager& s_resource_manager =
+        umpire::ResourceManager::getInstance();
+
+      return s_resource_manager;
+    }
+
+    static umpire::Allocator& GetDefaultHostAllocator()
+    {
+      static umpire::Allocator s_default_host_allocator =
+        GetResourceManager().getAllocator("HOST");
+
+      return s_default_host_allocator;
+    }
+
+    static umpire::Allocator& GetDefaultDeviceAllocator()
+    {
+      static umpire::Allocator s_default_device_allocator =
+        GetResourceManager().getAllocator("DEVICE");
+
+      return s_default_device_allocator;
+    }
+
+    static umpire::Allocator& GetCustomHostAllocator()
+    {
+      static umpire::Allocator s_custom_host_allocator =
+        GetResourceManager().makeAllocator<umpire::strategy::QuickPool>(
+          "HOST_CUSTOM", GetDefaultHostAllocator());
+
+      return s_custom_host_allocator;
+    }
+
+    static umpire::Allocator& GetCustomDeviceAllocator()
+    {
+      static umpire::Allocator s_custom_device_allocator =
+        GetResourceManager().makeAllocator<umpire::strategy::QuickPool>(
+          "DEVICE_CUSTOM", GetDefaultDeviceAllocator());
+
+      return s_custom_device_allocator;
+    }
+
+    void SetUp() override
+    {
+      GetContextManager().reset();
+    }
+
+    void TearDown() override
+    {
+      GetContextManager().reset();
+    }
+
+    void SetContextManagerState(ContextManagerState state)
+    {
+      chai::expt::ContextManager& context_manager = GetContextManager();
+      context_manager.reset();
+
+      if (state == ContextManagerState::CONTEXT_NONE_SYNCHRONIZED_DEVICE)
+      {
+        context_manager.setContext(chai::expt::Context::NONE);
+      }
+      else if (state == ContextManagerState::CONTEXT_HOST_SYNCHRONIZED_DEVICE)
+      {
+        context_manager.setContext(chai::expt::Context::HOST);
+      }
+      else if (state == ContextManagerState::CONTEXT_DEVICE_SYNCHRONIZED_DEVICE)
+      {
+        context_manager.setContext(chai::expt::Context::DEVICE);
+        context_manager.synchronize(chai::expt::Context::DEVICE);
+      }
+      else if (state == ContextManagerState::CONTEXT_NONE_UNSYNCHRONIZED_DEVICE)
+      {
+        context_manager.setContext(chai::expt::Context::DEVICE);
+        context_manager.setContext(chai::expt::Context::NONE);
+      }
+      else if (state == ContextManagerState::CONTEXT_HOST_UNSYNCHRONIZED_DEVICE)
+      {
+        context_manager.setContext(chai::expt::Context::DEVICE);
+        context_manager.setContext(chai::expt::Context::HOST);
+      }
+      else if (state == ContextManagerState::CONTEXT_DEVICE_UNSYNCHRONIZED_DEVICE)
+      {
+        context_manager.setContext(chai::expt::Context::DEVICE);
+      }
+    }
+
+    void SetArrayManagerState(ArrayManagerState)
+
+    std::size_t m_size = 10;
+};
+
+class HostDeviceArrayManager_AllocatedNone_Test : public ::testing::Test
+{
+  protected:
+    chai::expt::HostDeviceArrayManager<int> m_array{};
+};
+
+class HostDeviceArrayManager_AllocatedHost_Test : public ::testing::Test
+{
+  protected:
+    void SetUp() override
+    {
+      GetContextManager().reset();
+      GetContextManager().setContext(chai::expt::Context::HOST);
+      m_array.resize(m_size);
+      GetContextManager().reset();
+    }
+
+    void TearDown() override
+    {
+      GetContextManager().reset();
+      m_array.free();
+    }
+    
+    chai::expt::HostDeviceArrayManager<int> m_array{};
+};
+
+TEST_F(HostDeviceArrayManagerTest, DefaultConstructor)
+{
+  for (ContextManagerState context_manager_state : GetContextManagerStates())
+  {
+    SetContextManagerState(context_manager_state);
+    chai::expt::HostDeviceArrayManager<int> array;
+    EXPECT_EQ(array.size(), 0);
+    EXPECT_EQ(array.modified(), chai::expt::Context::NONE);
+    EXPECT_EQ(array.host_data(), nullptr);
+    EXPECT_EQ(array.device_data(), nullptr);
+    EXPECT_EQ(array.host_allocator().getId(), GetDefaultHostAllocator().getId());
+    EXPECT_EQ(array.device_allocator().getId(), GetDefaultDeviceAllocator().getId());
+  }
+}
+
+TEST_F(HostDeviceArrayManagerTest, AllocatorConstructor)
+{
+  for (ContextManagerState context_manager_state : GetContextManagerStates())
+  {
+    SetContextManagerState(context_manager_state);
+    chai::expt::HostDeviceArrayManager<int> array(GetCustomHostAllocator(), GetCustomDeviceAllocator());
+    EXPECT_EQ(array.size(), 0);
+    EXPECT_EQ(array.modified(), chai::expt::Context::NONE);
+    EXPECT_EQ(array.host_data(), nullptr);
+    EXPECT_EQ(array.device_data(), nullptr);
+    EXPECT_EQ(array.host_allocator().getId(), GetCustomHostAllocator().getId());
+    EXPECT_EQ(array.device_allocator().getId(), GetCustomDeviceAllocator().getId());
+  }
+}
+
+TEST_F(HostDeviceArrayManagerTest, SizeConstructor)
+{
+  for (ContextManagerState context_manager_state : GetContextManagerStates())
+  {
+    SetContextManagerState(context_manager_state);
+    chai::expt::HostDeviceArrayManager<int> array(m_size);
+    EXPECT_EQ(array.size(), m_size);
+    EXPECT_EQ(array.modified(), chai::expt::Context::NONE);
+    EXPECT_EQ(array.host_allocator().getId(), GetDefaultHostAllocator().getId());
+    EXPECT_EQ(array.device_allocator().getId(), GetDefaultDeviceAllocator().getId());
+
+    if (context_manager_state == ContextManagerState::CONTEXT_NONE_SYNCHRONIZED_DEVICE ||
+        context_manager_state == ContextManagerState::CONTEXT_NONE_UNSYNCHRONIZED_DEVICE)
+    {
+      EXPECT_EQ(array.host_data(), nullptr);
+      EXPECT_EQ(array.device_data(), nullptr);
+    }
+    else if (context_manager_state == ContextManagerState::CONTEXT_HOST_SYNCHRONIZED_DEVICE ||
+             context_manager_state == ContextManagerState::CONTEXT_HOST_UNSYNCHRONIZED_DEVICE)
+    {
+      EXPECT_NE(array.host_data(), nullptr);
+      ASSERT_TRUE(GetResourceManager().hasAllocator(array.host_data()));
+      EXPECT_EQ(GetResourceManager().getAllocator(array.host_data()).getId(), GetDefaultHostAllocator().getId());
+      EXPECT_EQ(array.device_data(), nullptr);
+    }
+    else if (context_manager_state == ContextManagerState::CONTEXT_DEVICE_SYNCHRONIZED_DEVICE ||
+             context_manager_state == ContextManagerState::CONTEXT_DEVICE_UNSYNCHRONIZED_DEVICE)
+    {
+      EXPECT_EQ(array.host_data(), nullptr);
+      EXPECT_NE(array.device_data(), nullptr);
+      ASSERT_TRUE(GetResourceManager().hasAllocator(array.device_data()));
+      EXPECT_EQ(GetResourceManager().getAllocator(array.device_data()).getId(), GetDefaultDeviceAllocator().getId());
+    }
+  }
+}
+
+TEST_F(HostDeviceArrayManagerTest, SizeAndAllocatorConstructor)
+{
+  for (auto context_manager_state : GetContextManagerStates())
+  {
+    chai::expt::HostDeviceArrayManager<int> array(m_size,
+                                     GetCustomHostAllocator(),
+                                     GetCustomDeviceAllocator());
+
+    EXPECT_EQ(array.size(), m_size);
+    EXPECT_EQ(array.modified(), chai::expt::Context::NONE);
+    EXPECT_EQ(array.host_allocator().getId(), GetCustomHostAllocator().getId());
+    EXPECT_EQ(array.device_allocator().getId(), GetCustomDeviceAllocator().getId());
+
+    if (context_manager_state == ContextManagerState::CONTEXT_NONE_SYNCHRONIZED_DEVICE ||
+        context_manager_state == ContextManagerState::CONTEXT_NONE_UNSYNCHRONIZED_DEVICE)
+    {
+      EXPECT_EQ(array.host_data(), nullptr);
+      EXPECT_EQ(array.device_data(), nullptr);
+    }
+    else if (context_manager_state == ContextManagerState::CONTEXT_HOST_SYNCHRONIZED_DEVICE ||
+             context_manager_state == ContextManagerState::CONTEXT_HOST_UNSYNCHRONIZED_DEVICE)
+    {
+      EXPECT_NE(array.host_data(), nullptr);
+      ASSERT_TRUE(GetResourceManager().hasAllocator(array.host_data()));
+      EXPECT_EQ(GetResourceManager().getAllocator(array.host_data()).getId(), GetCustomHostAllocator().getId());
+      EXPECT_EQ(array.device_data(), nullptr);
+    }
+    else if (context_manager_state == ContextManagerState::CONTEXT_DEVICE_SYNCHRONIZED_DEVICE ||
+             context_manager_state == ContextManagerState::CONTEXT_DEVICE_UNSYNCHRONIZED_DEVICE)
+    {
+      EXPECT_EQ(array.host_data(), nullptr);
+      EXPECT_NE(array.device_data(), nullptr);
+      ASSERT_TRUE(GetResourceManager().hasAllocator(array.device_data()));
+      EXPECT_EQ(GetResourceManager().getAllocator(array.device_data()).getId(), GetCustomDeviceAllocator().getId());
+    }
+  }
+}
+
+TEST_F(HostDeviceArrayManagerTest, CopyConstructor) {
+  const size_t size = 5;
+  chai::expt::HostDeviceArrayManager<int> array1(size, GetCustomHostAllocator(), GetCustomDeviceAllocator());
+  
+  // Set some data in array1
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::HOST);
+  for (size_t i = 0; i < size; ++i) {
+    array1.set(i, static_cast<int>(i));
+  }
+  
+  // Copy construct array2
+  chai::expt::HostDeviceArrayManager<int> array2(array1);
+  
+  EXPECT_EQ(array2.size(), size);
+  
+  // Verify data was copied
+  for (size_t i = 0; i < size; ++i) {
+    EXPECT_EQ(array2.get(i), static_cast<int>(i));
+  }
+}
+
+TEST_F(HostDeviceArrayManagerTest, MoveConstructor) {
+  const size_t size = 5;
+  chai::expt::HostDeviceArrayManager<int> array1(size, GetCustomHostAllocator(), GetCustomDeviceAllocator());
+  
+  // Set some data in array1
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::HOST);
+  for (size_t i = 0; i < size; ++i) {
+    array1.set(i, static_cast<int>(i));
+  }
+  
+  // Move construct array2
+  chai::expt::HostDeviceArrayManager<int> array2(std::move(array1));
+  
+  EXPECT_EQ(array2.size(), size);
+  EXPECT_EQ(array1.size(), 0);  // array1 should be empty after move
+  EXPECT_EQ(array1.host_data(), nullptr);
+  EXPECT_EQ(array1.device_data(), nullptr);
+  
+  // Verify data was moved
+  for (size_t i = 0; i < size; ++i) {
+    EXPECT_EQ(array2.get(i), static_cast<int>(i));
+  }
+}
+
+TEST_F(HostDeviceArrayManagerTest, CopyAssignment) {
+  const size_t size = 5;
+  chai::expt::HostDeviceArrayManager<int> array1(size, GetCustomHostAllocator(), GetCustomDeviceAllocator());
+  
+  // Set some data in array1
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::HOST);
+  for (size_t i = 0; i < size; ++i) {
+    array1.set(i, static_cast<int>(i));
+  }
+  
+  // Copy assign to array2
+  chai::expt::HostDeviceArrayManager<int> array2;
+  array2 = array1;
+  
+  EXPECT_EQ(array2.size(), size);
+  
+  // Verify data was copied
+  for (size_t i = 0; i < size; ++i) {
+    EXPECT_EQ(array2.get(i), static_cast<int>(i));
+  }
+}
+
+TEST_F(HostDeviceArrayManagerTest, MoveAssignment) {
+  const size_t size = 5;
+  chai::expt::HostDeviceArrayManager<int> array1(size, GetCustomHostAllocator(), GetCustomDeviceAllocator());
+  
+  // Set some data in array1
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::HOST);
+  for (size_t i = 0; i < size; ++i) {
+    array1.set(i, static_cast<int>(i));
+  }
+  
+  // Move assign to array2
+  chai::expt::HostDeviceArrayManager<int> array2;
+  array2 = std::move(array1);
+  
+  EXPECT_EQ(array2.size(), size);
+  EXPECT_EQ(array1.size(), 0);  // array1 should be empty after move
+  EXPECT_EQ(array1.host_data(), nullptr);
+  EXPECT_EQ(array1.device_data(), nullptr);
+  
+  // Verify data was moved
+  for (size_t i = 0; i < size; ++i) {
+    EXPECT_EQ(array2.get(i), static_cast<int>(i));
+  }
+}
+
+TEST_F(HostDeviceArrayManagerTest, Resize) {
+  chai::expt::HostDeviceArrayManager<int> array(5, GetCustomHostAllocator(), GetCustomDeviceAllocator());
+  EXPECT_EQ(array.size(), 5);
+  
+  // Resize larger
+  array.resize(10);
+  EXPECT_EQ(array.size(), 10);
+  
+  // Resize smaller
+  array.resize(3);
+  EXPECT_EQ(array.size(), 3);
+  
+  // Resize to zero
+  array.resize(0);
+  EXPECT_EQ(array.size(), 0);
+}
+
+TEST_F(HostDeviceArrayManagerTest, ResizeWithData) {
+  const size_t initial_size = 5;
+  chai::expt::HostDeviceArrayManager<int> array(initial_size, GetCustomHostAllocator(), GetCustomDeviceAllocator());
+  
+  // Set some data
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::HOST);
+  for (size_t i = 0; i < initial_size; ++i) {
+    array.set(i, static_cast<int>(i));
+  }
+  
+  // Resize larger
+  const size_t new_size = 8;
+  array.resize(new_size);
+  
+  // Verify original data is preserved
+  for (size_t i = 0; i < initial_size; ++i) {
+    EXPECT_EQ(array.get(i), static_cast<int>(i));
+  }
+  
+  // Resize smaller
+  const size_t smaller_size = 3;
+  array.resize(smaller_size);
+  
+  // Verify remaining data is preserved
+  for (size_t i = 0; i < smaller_size; ++i) {
+    EXPECT_EQ(array.get(i), static_cast<int>(i));
+  }
+}
+
+TEST_F(HostDeviceArrayManagerTest, Free) {
+  chai::expt::HostDeviceArrayManager<int> array(5, GetCustomHostAllocator(), GetCustomDeviceAllocator());
+  
+  array.free();
+  EXPECT_EQ(array.size(), 0);
+  EXPECT_EQ(array.host_data(), nullptr);
+  EXPECT_EQ(array.device_data(), nullptr);
+  EXPECT_EQ(array.modified(), chai::expt::Context::NONE);
+}
+
+TEST_F(HostDeviceArrayManagerTest, DataAndModified) {
+  const size_t size = 5;
+  chai::expt::HostDeviceArrayManager<int> array(size, GetCustomHostAllocator(), GetCustomDeviceAllocator());
+  
+  // Test host context
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::HOST);
+  int* host_ptr = array.data();
+  EXPECT_NE(host_ptr, nullptr);
+  EXPECT_EQ(array.modified(), chai::expt::Context::HOST);
+  
+  // Test device context
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::DEVICE);
+  int* device_ptr = array.data();
+  EXPECT_NE(device_ptr, nullptr);
+  EXPECT_EQ(array.modified(), chai::expt::Context::DEVICE);
+}
+
+TEST_F(HostDeviceArrayManagerTest, ConstData) {
+  const size_t size = 5;
+  chai::expt::HostDeviceArrayManager<int> array(size, GetCustomHostAllocator(), GetCustomDeviceAllocator());
+  
+  // Set some data
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::HOST);
+  for (size_t i = 0; i < size; ++i) {
+    array.set(i, static_cast<int>(i));
+  }
+  
+  // Create a const reference
+  const chai::expt::HostDeviceArrayManager<int>& const_array = array;
+  
+  // Test host context
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::HOST);
+  const int* host_ptr = const_array.data();
+  EXPECT_NE(host_ptr, nullptr);
+  
+  // Test device context
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::DEVICE);
+  const int* device_ptr = const_array.data();
+  EXPECT_NE(device_ptr, nullptr);
+}
+
+TEST_F(HostDeviceArrayManagerTest, GetAndSet) {
+  const size_t size = 5;
+  chai::expt::HostDeviceArrayManager<int> array(size, GetCustomHostAllocator(), GetCustomDeviceAllocator());
+  
+  // Set values in host context
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::HOST);
+  for (size_t i = 0; i < size; ++i) {
+    array.set(i, static_cast<int>(i * 10));
+  }
+  
+  // Get values in host context
+  for (size_t i = 0; i < size; ++i) {
+    EXPECT_EQ(array.get(i), static_cast<int>(i * 10));
+  }
+  
+  // Switch to device context and test data sync
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::DEVICE);
+  int* device_ptr = array.data();
+  
+  // Switch back to host and verify data still accessible
+  chai::expt::ContextManager::getInstance().setContext(chai::expt::Context::HOST);
+  for (size_t i = 0; i < size; ++i) {
+    EXPECT_EQ(array.get(i), static_cast<int>(i * 10));
+  }
+}
+
+TEST(HostDeviceArrayManager, DefaultConstructor_ContextNone_DeviceSynchronized)
+{
+
+  HostDeviceArrayManager<int> manager;
+
+
+}
