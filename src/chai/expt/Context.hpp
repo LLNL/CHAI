356cf33feecafaa43200d5e6add56a8988d89d99
Alan Dayton <dayton8@llnl.gov>
2026-01-09 09:25:26 -0800

    Add expt tests and update copyright for new files

--- src/chai/expt/Context.hpp
+++ src/chai/expt/Context.hpp
@@ -1,3 +1,10 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-26, Lawrence Livermore National Security, LLC and CHAI
+// project contributors. See the CHAI LICENSE file for details.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//////////////////////////////////////////////////////////////////////////////
+
 #ifndef CHAI_CONTEXT_HPP
 #define CHAI_CONTEXT_HPP
 
