ac9c77649a58356937725742ce1d2c382eb73ad7
Alan Dayton <dayton8@llnl.gov>
2026-01-23 10:59:34 -0800

    Add docs

--- src/chai/expt/HostArrayManager.hpp
+++ src/chai/expt/HostArrayManager.hpp
@@ -14,8 +14,39 @@
 
 namespace chai::expt
 {
+  /*!
+   * \brief This class manages a host array. It is designed for use with
+   *        ManagedArrayPointer.
+   *
+   * \tparam ElementType The type of elements contained in this array.
+   *
+   * \note HostArrayManager performs value initialization of each array element.
+   *       That is to say, numeric types will be initialized to zero and nontrivial
+   *       types will be default constructed. In the future, this behavior may
+   *       change to default initialization for performance reasons, such that
+   *       numeric types will be left in an indeterminate state and nontrivial
+   *       types will be default constructed.
+   *
+   * \note HostArrayManager does not rely on ContextManager, so it will behave
+   *       differently than other array managers. The major difference is that
+   *       when used with ManagedArrayPointer, the ManagedArrayPointer does not
+   *       need the update method called or to be copy constructed before it can
+   *       be used on the host. Since it will not respect the current Context,
+   *       be extra careful to avoid using it on the device.
+   */
   template <typename ElementType>
   class HostArrayManager {
+    private:
+      /*!
+       * \brief Allocator used by the managed host storage.
+       */
+      using AllocatorType = ::umpire::TypedAllocator<ElementType>;
+
+      /*!
+       * \brief Underlying contiguous host storage type for managed elements.
+       */
+      using StorageType = std::vector<ElementType, AllocatorType>;
+
     public:
       /*!
        * \brief Default-constructs a HostArrayManager with zero elements
@@ -30,7 +61,7 @@ namespace chai::expt
        * \param allocator Allocator used for host memory allocations.
        */
       explicit HostArrayManager(const umpire::Allocator& allocator)
-        : m_storage{std::vector<ElementType, ::umpire::TypedAllocator<ElementType>>(::umpire::TypedAllocator<ElementType>(allocator))}
+        : m_storage{StorageType(AllocatorType(allocator))}
       {
       }
 
@@ -41,7 +72,7 @@ namespace chai::expt
        * \param size Number of elements to allocate.
        */
       explicit HostArrayManager(std::size_t size)
-        : m_storage{std::vector<ElementType, ::umpire::TypedAllocator<ElementType>>(size, ::umpire::TypedAllocator<ElementType>(::umpire::ResourceManager::getInstance().getAllocator("HOST")))}
+        : m_storage{StorageType(size, AllocatorType(::umpire::ResourceManager::getInstance().getAllocator("HOST")))}
       {
       }
 
@@ -54,27 +85,45 @@ namespace chai::expt
        */
       HostArrayManager(std::size_t size,
                        const umpire::Allocator& allocator)
-        : m_storage{std::vector<ElementType, ::umpire::TypedAllocator<ElementType>>(size, ::umpire::TypedAllocator<ElementType>(allocator))}
+        : m_storage{StorageType(size, AllocatorType(allocator))}
       {
       }
 
+      /*!
+       * \brief Resizes the managed storage to \p new_size elements.
+       *
+       * \param new_size New number of elements.
+       */
       void resize(std::size_t new_size)
       {
         m_storage.resize(new_size);
       }
 
+      /*!
+       * \brief Returns the number of elements currently managed.
+       *
+       * \return Number of elements in the managed storage.
+       */
       std::size_t size() const
       {
         return m_storage.size();
       }
 
+      /*!
+       * \brief Returns a pointer to the underlying contiguous storage.
+       *
+       * \return Pointer to the first element, or nullptr if the storage is empty.
+       */
       ElementType* data()
       {
         return m_storage.empty() ? nullptr : m_storage.data();
       }
 
     private:
-      std::vector<ElementType, ::umpire::TypedAllocator<ElementType>> m_storage{::umpire::TypedAllocator<ElementType>(::umpire::ResourceManager::getInstance().getAllocator("HOST"))};
+      /*!
+       * \brief Underlying host storage for the managed elements.
+       */
+      StorageType m_storage{AllocatorType(::umpire::ResourceManager::getInstance().getAllocator("HOST"))};
   };  // class HostArrayManager
 }  // namespace chai::expt
 
