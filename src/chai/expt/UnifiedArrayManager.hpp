c9265413a8a84b0799904f5b5977f9a0f36a9f78
Alan Dayton <dayton8@llnl.gov>
2026-03-04 14:51:31 -0800

    Minor fixes

--- src/chai/expt/UnifiedArrayManager.hpp
+++ src/chai/expt/UnifiedArrayManager.hpp
@@ -1,9 +1,10 @@
 //////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2016-25, Lawrence Livermore National Security, LLC and CHAI
-// project contributors. See the CHAI LICENSE file for details.
+// Copyright (c) Lawrence Livermore National Security, LLC and other CHAI
+// contributors. See the CHAI LICENSE and COPYRIGHT files for details.
 //
 // SPDX-License-Identifier: BSD-3-Clause
 //////////////////////////////////////////////////////////////////////////////
+
 #ifndef CHAI_UNIFIED_ARRAY_MANAGER_HPP
 #define CHAI_UNIFIED_ARRAY_MANAGER_HPP
 
