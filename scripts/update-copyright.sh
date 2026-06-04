9a676c72a02c22c7add7661ef69d7b67f96a6c91
Alan Dayton <dayton8@llnl.gov>
2026-06-04 15:44:54 -0700

    Fix script

--- scripts/update-copyright.sh
+++ scripts/update-copyright.sh
@@ -12,7 +12,7 @@ set -euo pipefail
 NEW_END_YEAR="2026"
 echo "Updating copyright end year to ${NEW_END_YEAR}:"
 
-for file in "LICENSE" "docs/sphinx/conf.py" "docs/sphinx/conf.py" "README.md"; do
+for file in "LICENSE" "docs/sphinx/conf.py" "docs/sphinx/conf.py.in" "README.md"; do
     echo "  $file"
     sed -i "s/\([0-9]\{4\}\)-[0-9]\{4\}/\1-${NEW_END_YEAR}/g" "$file"
 done
